31cd9eca7285dbec683dfd115042916181ea955f
yama07 <n.116@icloud.com>
2021-10-25 14:22:10 +0900

    メインテーマを茶色系にした

--- renderer/lib/theme.ts
+++ renderer/lib/theme.ts
@@ -1,16 +1,17 @@
 import { createTheme } from "@material-ui/core/styles";
-import red from "@material-ui/core/colors/red";
+// import red from "@material-ui/core/colors/red";
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#00796B",
+      main: "#795548",
+      light: "#a98274",
+      dark: "#4b2c20",
     },
     secondary: {
-      main: "#FF9800",
-    },
-    error: {
-      main: red.A400,
+      main: "#ffa726",
+      light: "#ffd95b",
+      dark: "#c77800",
     },
     background: {
       default: "#F5F5F5",

a9eeb23dd7f3cfe8a9edfd884ba83635d9c5f67f
yama07 <n.116@icloud.com>
2022-03-14 00:03:20 +0900

    :white_check_mark: add renderer test base

---
+++ test/renderer/components/upstreamSettingDialogs/AddOrEditDialog.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+
+import AddOrEditDialog from "../../../../renderer/components/upstreamSettingDialogs/AddOrEditDialog";
+
+describe("AddOrEditDialogコンポーネント", () => {
+  test("要素が存在している", () => {
+    render(
+      <AddOrEditDialog
+        onConfirm={(newUpstream) => () => {}}
+        onDismiss={() => {}}
+      />
+    );
+
+    screen.getByText("Icon");
+    screen.getByText("Name");
+    screen.getByText("Host");
+    screen.getByText("Port number");
+    screen.getByText("Authentication");
+  });
+});

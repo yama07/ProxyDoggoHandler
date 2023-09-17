754753f9d25435d409b03709a91e2aed7af4967a
yama07 <n.116@icloud.com>
2023-09-17 16:41:02 +0900

    :passport_control: Switched to notarytool for notarization

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -17,10 +17,10 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: "jp.yama07.ProxyDoggoHandler",
+    tool: "notarytool",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: "@keychain:AC_PASSWORD",
-    ascProvider: process.env.ASC_PROVIDER,
+    teamId: process.env.TEAMID,
   });
 };

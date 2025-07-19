f67de1b8474544de299614e49dfcafa0396c1df1
yama07 <n.116@icloud.com>
2025-07-19 14:12:06 +0900

    :safety_vest: Proxy form validation migrated to zod

--- common/preference/proxyPreference.ts
+++ common/preference/proxyPreference.ts
@@ -2,10 +2,13 @@ import * as z from "zod";
 
 export const proxyPreferenceSchema = z.object({
   port: z
-    .number({ message: "このフィールドを入力してください。" })
+    .number({
+      required_error: "このフィールドを入力してください。",
+      invalid_type_error: "有効な値を入力してください。",
+    })
+    .int("有効な値を入力してください。")
     .min(0, "値は0以上にする必要があります。")
-    .max(65535, "値は65535以下にする必要があります。")
-    .default(8080),
+    .max(65535, "値は65535以下にする必要があります。"),
   verboseLogging: z.boolean(),
   isLaunchProxyServerAtStartup: z.boolean(),
 });
--- renderer/pages/preferences/proxy-server.tsx
+++ renderer/pages/preferences/proxy-server.tsx
@@ -1,8 +1,9 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, Checkbox, Divider, FormControlLabel, Grid, TextField } from "@mui/material";
 import { useCallback, useContext, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 
-import type { ProxyPreference } from "$/preference/proxyPreference";
+import { type ProxyPreference, proxyPreferenceSchema } from "$/preference/proxyPreference";
 
 import ProxyUsageCard from "~/components/ProxyUsageCard";
 import { proxyPrefContext, setProxyPrefContext } from "~/contexts/ProxyPrefContext";
@@ -17,6 +18,7 @@ const ProxyServer: React.FC = () => {
     criteriaMode: "all",
     shouldUseNativeValidation: false,
     defaultValues: proxyPref,
+    resolver: zodResolver(proxyPreferenceSchema),
   });
 
   const onApply = useCallback(
@@ -41,33 +43,23 @@ const ProxyServer: React.FC = () => {
             <Controller
               control={control}
               name="port"
-              rules={{
-                required: "このフィールドを入力してください。",
-                pattern: {
-                  value: /^[0-9]+$/,
-                  message: "数字を入力してください。",
-                },
-                min: {
-                  value: 0,
-                  message: "値は0以上にする必要があります。",
-                },
-                max: {
-                  value: 65535,
-                  message: "値は65535以下にする必要があります。",
-                },
-              }}
               render={({ field, fieldState: { error } }) => (
                 <TextField
                   {...field}
                   variant="standard"
                   margin="dense"
                   label="リクエストを受け付けるポート番号"
-                  type="number"
-                  InputLabelProps={{ shrink: true }}
-                  InputProps={{ inputProps: { min: 0, max: 65535 } }}
-                  error={error != null}
-                  helperText={error != null ? error.message : null}
+                  placeholder="8080"
+                  error={!!error}
+                  helperText={error?.message}
                   fullWidth
+                  onChange={(e) =>
+                    field.onChange(
+                      !e.target.value || Number.isNaN(Number(e.target.value))
+                        ? e.target.value
+                        : Number(e.target.value),
+                    )
+                  }
                 />
               )}
             />

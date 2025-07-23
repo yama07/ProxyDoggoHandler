ac2298b4980d471d7dcb41a32a490488351c1aa5
yama07 <n.116@icloud.com>
2025-07-23 22:58:47 +0900

    :arrow_up: Bump @electron/notarize from 2.2.0 to 3.0.1

--- scripts/notarize.mjs
+++ scripts/notarize.mjs
@@ -1,23 +1,22 @@
-process.env.DEBUG = "@electron/notarize*";
+import { notarize } from "@electron/notarize";
 
-const { notarize } = require("@electron/notarize");
+process.env.DEBUG = "electron-notarize*";
 
-exports.default = async function notarizing(context) {
+export default async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
 
-  // macOSではない、あるいはENABLE_NOTARIZEフラグが
-  // 有効ではない場合は公証を行わない
+  // macOSではない、あるいはENABLE_NOTARIZEフラグが有効ではない場合は公証を行わない
   if (electronPlatformName !== "darwin" || process.env.ENABLE_NOTARIZE === undefined) {
+    console.log("skipped macOS notarization in afterSign.");
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    tool: "notarytool",
+  await notarize({
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
     teamId: process.env.TEAMID,
   });
-};
+}
